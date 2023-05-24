ad2de354f95ae435b7ece1581fdb80d6ab5f9053
RenéMartinek <r.martinek90@gmail.com>
2023-05-24 14:42:17 +0200

    Added: Selection Component for Indoor or Outdoor Activity

--- activitymatcher/src/app/app.module.ts
+++ activitymatcher/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SelectionIndoorOutdoorComponent } from './selection-indoor-outdoor/selection-indoor-outdoor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    NavbarComponent
+    NavbarComponent,
+    SelectionIndoorOutdoorComponent
   ],
   imports: [
     BrowserModule,
---
+++ activitymatcher/src/app/selection-indoor-outdoor/selection-indoor-outdoor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-selection-indoor-outdoor',
+  templateUrl: './selection-indoor-outdoor.component.html',
+  styleUrls: ['./selection-indoor-outdoor.component.css']
+})
+export class SelectionIndoorOutdoorComponent {
+
+}
