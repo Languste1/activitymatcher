c8718fc46678b6b640dddc7b06051dfa7663f313
ChristianLange <christian.lange@brightstraining.com>
2023-05-25 12:44:20 +0200

    add: - new component - design of match page

---
+++ activitymatcher/src/app/activity-match/activity-match.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-match',
+  templateUrl: './activity-match.component.html',
+  styleUrls: ['./activity-match.component.css']
+})
+export class ActivityMatchComponent {
+
+}
--- activitymatcher/src/app/app-routing.module.ts
+++ activitymatcher/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {LandingPageComponent} from "./landing-page/landing-page.component";
 import {SelectionIndoorOutdoorComponent} from "./selection-indoor-outdoor/selection-indoor-outdoor.component";
 import {ActivitySwipeComponent} from "./activity-swipe/activity-swipe.component";
+import {ActivityMatchComponent} from "./activity-match/activity-match.component";
 
 const routes: Routes = [
   {path: "", component: LandingPageComponent},
   {path: "swipe", component: ActivitySwipeComponent},
-  {path: "selection", component: SelectionIndoorOutdoorComponent}
+  {path: "selection", component: SelectionIndoorOutdoorComponent},
+  {path: "match", component: ActivityMatchComponent}
 
 
 ];
