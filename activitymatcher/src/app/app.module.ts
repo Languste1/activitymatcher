17f96cd7920ab7987972a7dd4d6d814c66e83d5a
ChristianLange <christian.lange@brightstraining.com>
2023-06-06 11:47:36 +0200

    add: - delete wrong node_modules

--- activitymatcher/src/app/app.module.ts
+++ activitymatcher/src/app/app.module.ts
@@ -10,8 +10,8 @@ import { ActivityComponent } from './activity/activity.component';
 import { SelectionIndoorOutdoorComponent } from './selection-indoor-outdoor/selection-indoor-outdoor.component';
 import { ActivityMatchComponent } from './activity-match/activity-match.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-import {FormsModule} from "@angular/forms";
 import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
